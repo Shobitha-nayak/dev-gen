dbafead289dfc67d88ad4faa69befdc99891b704
Shobitha-nayak <68498429+Shobitha-nayak@users.noreply.github.com>
2024-10-21 13:52:32 +0530

    generate_report.py

--- scripts/generate_report.py
+++ scripts/generate_report.py
@@ -101,16 +101,37 @@
 
 
 
+# import pdfkit
+# import matplotlib.pyplot as plt
+
+# # Generate a simple HTML report
+# html = '''
+# <h1>DevOps Maturity Report</h1>
+# <p>Build Success Rate: 90%</p>
+# <p>Code Churn vs Test Coverage:</p>
+# <img src="plot.png">
+# '''
+
+# # Generate a plot for report
+# data = {
+#     'code_churn': [100, 50, 200, 30],
+#     'test_coverage': [80, 90, 70, 60]
+# }
+# plt.scatter(data['code_churn'], data['test_coverage'])
+# plt.title("Code Churn vs Test Coverage")
+# plt.xlabel("Code Churn")
+# plt.ylabel("Test Coverage")
+# plt.savefig('plot.png')
+
+# # Convert HTML to PDF
+# pdfkit.from_string(html, 'devops_maturity_report.pdf')
+
+
+
+
 import pdfkit
 import matplotlib.pyplot as plt
-
-# Generate a simple HTML report
-html = '''
-<h1>DevOps Maturity Report</h1>
-<p>Build Success Rate: 90%</p>
-<p>Code Churn vs Test Coverage:</p>
-<img src="plot.png">
-'''
+import os
 
 # Generate a plot for report
 data = {
@@ -122,7 +143,17 @@ plt.title("Code Churn vs Test Coverage")
 plt.xlabel("Code Churn")
 plt.ylabel("Test Coverage")
 plt.savefig('plot.png')
+plt.close()  # Close the plot to free up memory
+
+# Generate a simple HTML report
+html = f'''
+<h1>DevOps Maturity Report</h1>
+<p>Build Success Rate: 90%</p>
+<p>Code Churn vs Test Coverage:</p>
+<img src="{os.path.abspath('plot.png')}">
+'''
 
 # Convert HTML to PDF
 pdfkit.from_string(html, 'devops_maturity_report.pdf')
+
 
